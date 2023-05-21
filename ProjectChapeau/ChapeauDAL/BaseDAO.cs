22b23dd3657c8725c6e20ed72d4e4d07067f148d
jonathan-mauricio <jonathanmauricionl@gmail.com>
2023-05-21 02:51:50 +0200

    Initial Database Connection

---
+++ ProjectChapeau/ChapeauDAL/BaseDAO.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ChapeauDAL
+{
+    public class BaseDAO
+    {
+        private SqlDataAdapter adapter;
+        private SqlConnection conn;
+        public BaseDAO()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Chapeau Project"].ConnectionString);
+            adapter = new SqlDataAdapter();
+        }
+
+        protected SqlConnection OpenConnection()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed || conn.State == ConnectionState.Broken)
+                {
+                    conn.Open();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return conn;
+        }
+
+        private void CloseConnection()
+        {
+            conn.Close();
+        }
+
+        /* For Insert/Update/Delete Queries with transaction */
+        protected void ExecuteEditTranQuery(string query, SqlParameter[] sqlParameters, SqlTransaction sqlTransaction)
+        {
+            SqlCommand command = new SqlCommand(query, conn, sqlTransaction);
+            try
+            {
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /* For Insert/Update/Delete Queries */
+        protected void ExecuteEditQuery(string query, SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /* For Select Queries */
+        protected DataTable ExecuteSelectQuery(string query, params SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+            DataTable dataTable;
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                command.ExecuteNonQuery();
+                adapter.SelectCommand = command;
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return dataTable;
+        }
+    }
+}
