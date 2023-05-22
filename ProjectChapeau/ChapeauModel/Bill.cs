bfb373ca8297b44350ff40b14445d6ce319210e2
jonathan-mauricio <90071775+jonathan-mauricio@users.noreply.github.com>
2023-05-22 12:22:37 +0200

    more models

---
+++ ProjectChapeau/ChapeauModel/Bill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class Bill
+    {
+        public int BillId { get; set; }
+        public Table Table { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal TotalTip { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime date { get; set; }
+    }
+}
---
+++ ProjectChapeau/ChapeauModel/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class Table
+    {
+        public int TableId { get; set; }
+        public string Status { get; set; }
+        public int Number { get; set; }
+    }
+}
