7c328cbe181d2cba52ee99be2991c56836640984
Zoran Serdic <35991493+ZoranSerdic@users.noreply.github.com>
2023-05-23 12:11:01 +0200

    Created buttons in LoginView.cs
    So everyone can work without constantly changing the Program.cs code to test their form

--- ProjectChapeau/ChapeauUI/LoginView.Designer.cs
+++ ProjectChapeau/ChapeauUI/LoginView.Designer.cs
@@ -28,33 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnTableView = new System.Windows.Forms.Button();
+            this.btnOrderView = new System.Windows.Forms.Button();
+            this.btnBarKitchenView = new System.Windows.Forms.Button();
+            this.btnPaymentView = new System.Windows.Forms.Button();
+            this.btnManagerView = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnLogin
+            // btnTableView
             // 
-            this.btnLogin.Location = new System.Drawing.Point(325, 313);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(94, 29);
-            this.btnLogin.TabIndex = 0;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnTableView.Location = new System.Drawing.Point(316, 131);
+            this.btnTableView.Name = "btnTableView";
+            this.btnTableView.Size = new System.Drawing.Size(153, 29);
+            this.btnTableView.TabIndex = 0;
+            this.btnTableView.Text = "Table View";
+            this.btnTableView.UseVisualStyleBackColor = true;
+            this.btnTableView.Click += new System.EventHandler(this.btnTableView_Click);
+            // 
+            // btnOrderView
+            // 
+            this.btnOrderView.Location = new System.Drawing.Point(316, 166);
+            this.btnOrderView.Name = "btnOrderView";
+            this.btnOrderView.Size = new System.Drawing.Size(153, 29);
+            this.btnOrderView.TabIndex = 1;
+            this.btnOrderView.Text = "Order View";
+            this.btnOrderView.UseVisualStyleBackColor = true;
+            this.btnOrderView.Click += new System.EventHandler(this.btnOrderView_Click);
+            // 
+            // btnBarKitchenView
+            // 
+            this.btnBarKitchenView.Location = new System.Drawing.Point(316, 201);
+            this.btnBarKitchenView.Name = "btnBarKitchenView";
+            this.btnBarKitchenView.Size = new System.Drawing.Size(153, 29);
+            this.btnBarKitchenView.TabIndex = 2;
+            this.btnBarKitchenView.Text = "Bar Kitchen View";
+            this.btnBarKitchenView.UseVisualStyleBackColor = true;
+            this.btnBarKitchenView.Click += new System.EventHandler(this.btnBarKitchenView_Click);
+            // 
+            // btnPaymentView
+            // 
+            this.btnPaymentView.Location = new System.Drawing.Point(316, 236);
+            this.btnPaymentView.Name = "btnPaymentView";
+            this.btnPaymentView.Size = new System.Drawing.Size(153, 29);
+            this.btnPaymentView.TabIndex = 3;
+            this.btnPaymentView.Text = "Payment View";
+            this.btnPaymentView.UseVisualStyleBackColor = true;
+            this.btnPaymentView.Click += new System.EventHandler(this.btnPaymentView_Click);
+            // 
+            // btnManagerView
+            // 
+            this.btnManagerView.Location = new System.Drawing.Point(316, 271);
+            this.btnManagerView.Name = "btnManagerView";
+            this.btnManagerView.Size = new System.Drawing.Size(153, 29);
+            this.btnManagerView.TabIndex = 5;
+            this.btnManagerView.Text = "Manager View";
+            this.btnManagerView.UseVisualStyleBackColor = true;
+            this.btnManagerView.Click += new System.EventHandler(this.btnManagerView_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "This is the temporary login view";
             // 
             // LoginView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnManagerView);
+            this.Controls.Add(this.btnPaymentView);
+            this.Controls.Add(this.btnBarKitchenView);
+            this.Controls.Add(this.btnOrderView);
+            this.Controls.Add(this.btnTableView);
             this.Name = "LoginView";
             this.Text = "LoginView";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button btnLogin;
+        private Button btnTableView;
+        private Button btnOrderView;
+        private Button btnBarKitchenView;
+        private Button btnPaymentView;
+        private Button btnManagerView;
+        private Label label1;
     }
 }
--- ProjectChapeau/ChapeauUI/LoginView.cs
+++ ProjectChapeau/ChapeauUI/LoginView.cs
@@ -17,12 +17,44 @@ namespace ChapeauUI
             InitializeComponent();
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        private void btnTableView_Click(object sender, EventArgs e)
         {
             //Use this way to open the other forms in this order
-            TableView tableview = new TableView();
+            TableView tableView = new TableView();
             this.Hide();
-            tableview.ShowDialog();
+            tableView.ShowDialog();
+            this.Close();
+        }
+
+        private void btnOrderView_Click(object sender, EventArgs e)
+        {
+            OrderView orderView = new OrderView();
+            this.Hide();
+            orderView.ShowDialog();
+            this.Close();
+        }
+
+        private void btnBarKitchenView_Click(object sender, EventArgs e)
+        {
+            BarKitchenView barKitchenView = new BarKitchenView();
+            this.Hide();
+            barKitchenView.ShowDialog();
+            this.Close();
+        }
+
+        private void btnPaymentView_Click(object sender, EventArgs e)
+        {
+            PaymentView paymentView = new PaymentView();
+            this.Hide();
+            paymentView.ShowDialog();
+            this.Close();
+        }
+
+        private void btnManagerView_Click(object sender, EventArgs e)
+        {
+            ManagerView managerView = new ManagerView();
+            this.Hide();
+            managerView.ShowDialog();
             this.Close();
         }
     }
