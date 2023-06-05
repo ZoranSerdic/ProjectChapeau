594b114445aa4bda754fe83cd231952d741ce8ff
Zoran Serdic <35991493+ZoranSerdic@users.noreply.github.com>
2023-06-05 16:09:09 +0200

    OrderView Design

--- ProjectChapeau/ChapeauUI/OrderView.Designer.cs
+++ ProjectChapeau/ChapeauUI/OrderView.Designer.cs
@@ -28,12 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrderView));
+            this.labelTableNumber = new System.Windows.Forms.Label();
+            this.panelMenuBackground = new System.Windows.Forms.Panel();
+            this.labelMenuSeparator = new System.Windows.Forms.Label();
+            this.labelMenuTime = new System.Windows.Forms.Label();
+            this.labelMenuType = new System.Windows.Forms.Label();
+            this.panelGreenSeparatorOne = new System.Windows.Forms.Panel();
+            this.panelGreenSeparatorTwo = new System.Windows.Forms.Panel();
+            this.panelGreenSeparatorThree = new System.Windows.Forms.Panel();
+            this.pictureBoxChapeauLogo = new System.Windows.Forms.PictureBox();
+            this.buttonCategoryStarters = new System.Windows.Forms.Button();
+            this.buttonCategoryMainDish = new System.Windows.Forms.Button();
+            this.buttonCategoryDesserts = new System.Windows.Forms.Button();
+            this.buttonCategoryDrinks = new System.Windows.Forms.Button();
+            this.buttonSwitchMenu = new System.Windows.Forms.Button();
+            this.buttonCloseOrder = new System.Windows.Forms.Button();
+            this.buttonFinaliseOrder = new System.Windows.Forms.Button();
+            this.panelWhiteButtonBackground = new System.Windows.Forms.Panel();
+            this.listViewOrders = new System.Windows.Forms.ListView();
+            this.panelMenuBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChapeauLogo)).BeginInit();
+            this.panelWhiteButtonBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTableNumber
+            // 
+            this.labelTableNumber.AutoSize = true;
+            this.labelTableNumber.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTableNumber.Location = new System.Drawing.Point(61, 9);
+            this.labelTableNumber.Name = "labelTableNumber";
+            this.labelTableNumber.Size = new System.Drawing.Size(129, 45);
+            this.labelTableNumber.TabIndex = 0;
+            this.labelTableNumber.Text = "Table 1";
+            // 
+            // panelMenuBackground
+            // 
+            this.panelMenuBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.panelMenuBackground.Controls.Add(this.labelMenuSeparator);
+            this.panelMenuBackground.Controls.Add(this.labelMenuTime);
+            this.panelMenuBackground.Controls.Add(this.labelMenuType);
+            this.panelMenuBackground.Location = new System.Drawing.Point(12, 69);
+            this.panelMenuBackground.Name = "panelMenuBackground";
+            this.panelMenuBackground.Size = new System.Drawing.Size(508, 41);
+            this.panelMenuBackground.TabIndex = 1;
+            // 
+            // labelMenuSeparator
+            // 
+            this.labelMenuSeparator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMenuSeparator.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMenuSeparator.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelMenuSeparator.Location = new System.Drawing.Point(242, -4);
+            this.labelMenuSeparator.Name = "labelMenuSeparator";
+            this.labelMenuSeparator.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelMenuSeparator.Size = new System.Drawing.Size(25, 40);
+            this.labelMenuSeparator.TabIndex = 4;
+            this.labelMenuSeparator.Text = "-";
+            this.labelMenuSeparator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelMenuTime
+            // 
+            this.labelMenuTime.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelMenuTime.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMenuTime.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelMenuTime.Location = new System.Drawing.Point(268, -4);
+            this.labelMenuTime.Name = "labelMenuTime";
+            this.labelMenuTime.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelMenuTime.Size = new System.Drawing.Size(240, 40);
+            this.labelMenuTime.TabIndex = 3;
+            this.labelMenuTime.Text = "Diner";
+            this.labelMenuTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelMenuType
+            // 
+            this.labelMenuType.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelMenuType.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMenuType.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelMenuType.Location = new System.Drawing.Point(0, -4);
+            this.labelMenuType.Name = "labelMenuType";
+            this.labelMenuType.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelMenuType.Size = new System.Drawing.Size(240, 40);
+            this.labelMenuType.TabIndex = 2;
+            this.labelMenuType.Text = "Starters";
+            this.labelMenuType.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panelGreenSeparatorOne
+            // 
+            this.panelGreenSeparatorOne.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(115)))), ((int)(((byte)(26)))));
+            this.panelGreenSeparatorOne.Location = new System.Drawing.Point(0, 113);
+            this.panelGreenSeparatorOne.Name = "panelGreenSeparatorOne";
+            this.panelGreenSeparatorOne.Size = new System.Drawing.Size(532, 11);
+            this.panelGreenSeparatorOne.TabIndex = 2;
+            // 
+            // panelGreenSeparatorTwo
+            // 
+            this.panelGreenSeparatorTwo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(115)))), ((int)(((byte)(26)))));
+            this.panelGreenSeparatorTwo.Location = new System.Drawing.Point(0, 657);
+            this.panelGreenSeparatorTwo.Name = "panelGreenSeparatorTwo";
+            this.panelGreenSeparatorTwo.Size = new System.Drawing.Size(532, 11);
+            this.panelGreenSeparatorTwo.TabIndex = 3;
+            // 
+            // panelGreenSeparatorThree
+            // 
+            this.panelGreenSeparatorThree.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(115)))), ((int)(((byte)(26)))));
+            this.panelGreenSeparatorThree.Location = new System.Drawing.Point(0, 810);
+            this.panelGreenSeparatorThree.Name = "panelGreenSeparatorThree";
+            this.panelGreenSeparatorThree.Size = new System.Drawing.Size(532, 11);
+            this.panelGreenSeparatorThree.TabIndex = 4;
+            // 
+            // pictureBoxChapeauLogo
+            // 
+            this.pictureBoxChapeauLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxChapeauLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxChapeauLogo.Image")));
+            this.pictureBoxChapeauLogo.Location = new System.Drawing.Point(210, 7);
+            this.pictureBoxChapeauLogo.Name = "pictureBoxChapeauLogo";
+            this.pictureBoxChapeauLogo.Size = new System.Drawing.Size(102, 58);
+            this.pictureBoxChapeauLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxChapeauLogo.TabIndex = 17;
+            this.pictureBoxChapeauLogo.TabStop = false;
+            // 
+            // buttonCategoryStarters
+            // 
+            this.buttonCategoryStarters.AutoSize = true;
+            this.buttonCategoryStarters.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(67)))), ((int)(((byte)(183)))));
+            this.buttonCategoryStarters.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCategoryStarters.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCategoryStarters.Location = new System.Drawing.Point(30, 14);
+            this.buttonCategoryStarters.Name = "buttonCategoryStarters";
+            this.buttonCategoryStarters.Size = new System.Drawing.Size(222, 64);
+            this.buttonCategoryStarters.TabIndex = 18;
+            this.buttonCategoryStarters.Text = "Starters";
+            this.buttonCategoryStarters.UseVisualStyleBackColor = false;
+            // 
+            // buttonCategoryMainDish
+            // 
+            this.buttonCategoryMainDish.AutoSize = true;
+            this.buttonCategoryMainDish.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(67)))), ((int)(((byte)(183)))));
+            this.buttonCategoryMainDish.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCategoryMainDish.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCategoryMainDish.Location = new System.Drawing.Point(280, 14);
+            this.buttonCategoryMainDish.Name = "buttonCategoryMainDish";
+            this.buttonCategoryMainDish.Size = new System.Drawing.Size(222, 64);
+            this.buttonCategoryMainDish.TabIndex = 19;
+            this.buttonCategoryMainDish.Text = "Main Dish";
+            this.buttonCategoryMainDish.UseVisualStyleBackColor = false;
+            // 
+            // buttonCategoryDesserts
+            // 
+            this.buttonCategoryDesserts.AutoSize = true;
+            this.buttonCategoryDesserts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(67)))), ((int)(((byte)(183)))));
+            this.buttonCategoryDesserts.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCategoryDesserts.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCategoryDesserts.Location = new System.Drawing.Point(30, 80);
+            this.buttonCategoryDesserts.Name = "buttonCategoryDesserts";
+            this.buttonCategoryDesserts.Size = new System.Drawing.Size(222, 64);
+            this.buttonCategoryDesserts.TabIndex = 20;
+            this.buttonCategoryDesserts.Text = "Desserts";
+            this.buttonCategoryDesserts.UseVisualStyleBackColor = false;
+            // 
+            // buttonCategoryDrinks
+            // 
+            this.buttonCategoryDrinks.AutoSize = true;
+            this.buttonCategoryDrinks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(67)))), ((int)(((byte)(183)))));
+            this.buttonCategoryDrinks.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCategoryDrinks.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCategoryDrinks.Location = new System.Drawing.Point(280, 80);
+            this.buttonCategoryDrinks.Name = "buttonCategoryDrinks";
+            this.buttonCategoryDrinks.Size = new System.Drawing.Size(222, 64);
+            this.buttonCategoryDrinks.TabIndex = 21;
+            this.buttonCategoryDrinks.Text = "Drinks";
+            this.buttonCategoryDrinks.UseVisualStyleBackColor = false;
+            // 
+            // buttonSwitchMenu
+            // 
+            this.buttonSwitchMenu.AutoSize = true;
+            this.buttonSwitchMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(67)))), ((int)(((byte)(183)))));
+            this.buttonSwitchMenu.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSwitchMenu.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSwitchMenu.Location = new System.Drawing.Point(64, 827);
+            this.buttonSwitchMenu.Name = "buttonSwitchMenu";
+            this.buttonSwitchMenu.Size = new System.Drawing.Size(388, 56);
+            this.buttonSwitchMenu.TabIndex = 22;
+            this.buttonSwitchMenu.Text = "Switch To Lunch Menu";
+            this.buttonSwitchMenu.UseVisualStyleBackColor = false;
+            // 
+            // buttonCloseOrder
+            // 
+            this.buttonCloseOrder.AutoSize = true;
+            this.buttonCloseOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.buttonCloseOrder.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCloseOrder.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCloseOrder.Location = new System.Drawing.Point(12, 885);
+            this.buttonCloseOrder.Name = "buttonCloseOrder";
+            this.buttonCloseOrder.Size = new System.Drawing.Size(217, 56);
+            this.buttonCloseOrder.TabIndex = 23;
+            this.buttonCloseOrder.Text = "Close Order";
+            this.buttonCloseOrder.UseVisualStyleBackColor = false;
+            // 
+            // buttonFinaliseOrder
+            // 
+            this.buttonFinaliseOrder.AutoSize = true;
+            this.buttonFinaliseOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(115)))), ((int)(((byte)(26)))));
+            this.buttonFinaliseOrder.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonFinaliseOrder.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonFinaliseOrder.Location = new System.Drawing.Point(268, 885);
+            this.buttonFinaliseOrder.Name = "buttonFinaliseOrder";
+            this.buttonFinaliseOrder.Size = new System.Drawing.Size(252, 56);
+            this.buttonFinaliseOrder.TabIndex = 0;
+            this.buttonFinaliseOrder.Text = "Finalise Order";
+            this.buttonFinaliseOrder.UseVisualStyleBackColor = false;
+            // 
+            // panelWhiteButtonBackground
+            // 
+            this.panelWhiteButtonBackground.BackColor = System.Drawing.Color.White;
+            this.panelWhiteButtonBackground.Controls.Add(this.buttonCategoryStarters);
+            this.panelWhiteButtonBackground.Controls.Add(this.buttonCategoryMainDish);
+            this.panelWhiteButtonBackground.Controls.Add(this.buttonCategoryDrinks);
+            this.panelWhiteButtonBackground.Controls.Add(this.buttonCategoryDesserts);
+            this.panelWhiteButtonBackground.Location = new System.Drawing.Point(0, 660);
+            this.panelWhiteButtonBackground.Name = "panelWhiteButtonBackground";
+            this.panelWhiteButtonBackground.Size = new System.Drawing.Size(532, 158);
+            this.panelWhiteButtonBackground.TabIndex = 4;
+            // 
+            // listViewOrders
+            // 
+            this.listViewOrders.Location = new System.Drawing.Point(12, 130);
+            this.listViewOrders.Name = "listViewOrders";
+            this.listViewOrders.Size = new System.Drawing.Size(508, 535);
+            this.listViewOrders.TabIndex = 24;
+            this.listViewOrders.UseCompatibleStateImageBehavior = false;
+            // 
+            // OrderView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(234)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(532, 953);
+            this.Controls.Add(this.buttonFinaliseOrder);
+            this.Controls.Add(this.buttonCloseOrder);
+            this.Controls.Add(this.buttonSwitchMenu);
+            this.Controls.Add(this.pictureBoxChapeauLogo);
+            this.Controls.Add(this.panelGreenSeparatorThree);
+            this.Controls.Add(this.panelGreenSeparatorTwo);
+            this.Controls.Add(this.panelGreenSeparatorOne);
+            this.Controls.Add(this.listViewOrders);
+            this.Controls.Add(this.panelWhiteButtonBackground);
+            this.Controls.Add(this.panelMenuBackground);
+            this.Controls.Add(this.labelTableNumber);
+            this.Name = "OrderView";
             this.Text = "OrderView";
+            this.panelMenuBackground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChapeauLogo)).EndInit();
+            this.panelWhiteButtonBackground.ResumeLayout(false);
+            this.panelWhiteButtonBackground.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label labelTableNumber;
+        private Panel panelMenuBackground;
+        private Label labelMenuType;
+        private Label labelMenuTime;
+        private Label labelMenuSeparator;
+        private Panel panelGreenSeparatorOne;
+        private Panel panelGreenSeparatorTwo;
+        private Panel panelGreenSeparatorThree;
+        private PictureBox pictureBoxChapeauLogo;
+        private Button buttonCategoryStarters;
+        private Button buttonCategoryMainDish;
+        private Button buttonCategoryDesserts;
+        private Button buttonCategoryDrinks;
+        private Button buttonSwitchMenu;
+        private Button buttonCloseOrder;
+        private Button buttonFinaliseOrder;
+        private Panel panelWhiteButtonBackground;
+        private ListView listViewOrders;
     }
 }
